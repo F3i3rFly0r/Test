771bd1a04196d5a6b8551f46793913f1fa5f9ee4
F3i3rFly0r <github@ascavetius.de>
2011-10-03 17:58:20 +0200

    vorLayout

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Liefert einen Titel pro Seite
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
